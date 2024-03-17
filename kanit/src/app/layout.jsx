e743ef1cef3bcac1b45e211334bc8785f1ac12b9
ShardulVanage <vsharadul@gmail.com>
2024-03-17 13:04:27 +0530

    favicon

--- kanit/src/app/layout.jsx
+++ kanit/src/app/layout.jsx
@@ -4,8 +4,8 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    template: 'Kanit Media',
+    default: 'Kanit Media - Award winning developer studio based in Denmark',
   },
 }
 
